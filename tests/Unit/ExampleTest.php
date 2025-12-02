ea39342ec1bcc5dd88cb3989e0b3424261e0adc7
Leviek <hemmanuelmtz777@gmail.com>
2025-12-02 15:23:54 -0600

    Create dev branch

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,5 +1,6 @@
 <?php
 
+//Aqui van las pruebas de sus controllers, todos los controlladores deben llevar la palabra Controller
 test('that true is true', function () {
     expect(true)->toBeTrue();
 });
