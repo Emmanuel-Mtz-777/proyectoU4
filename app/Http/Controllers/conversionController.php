a1966895c1dae4e39343cdc9abbba8a7b770d325
AngelOmarNegreteDemetrio <negretedemetrioangel@gmail.com>
2025-12-02 21:06:46 -0600

    se realizo actualizacion de cambios en dev

--- app/Http/Controllers/conversionController.php
+++ app/Http/Controllers/conversionController.php
@@ -4,12 +4,12 @@ namespace App\Http\Controllers;
 
 class conversionController extends Controller
 {
-    public function cToF($c)
+    public function cToF(float $c): float 
     {
         return ($c * 9 / 5) + 32;
     }
 
-    public function mbToGb($mb)
+    public function mbToGb(float $mb): float
     {
         return $mb / 1024;
     }
